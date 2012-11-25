9c9284d0f8ea772f8cb52b506af031ab07c7c845
Justin Huff <jjhuff@mspin.net>
2012-11-24 22:36:00 -0800

    Unit conversion

--- appengine/static/js/plot.js
+++ appengine/static/js/plot.js
@@ -3,6 +3,31 @@ var SNOW_HEIGHT_ADJ = '#C79F81'
 var SNOW_TEMP = '#81F79F'
 var AIR_TEMP = '#81DAF5'
 
+var UNITS = {
+    metric: {
+        temp: {
+            format: '##.#\u00B0C',
+            convert: function(v){ return v }
+        },
+        dist: {
+            format: '###.#cm',
+            convert: function(v){ return v }
+        }
+    },
+    imperial: {
+        temp: {
+            format: '##.#\u00B0F',
+            convert: function(v){ return v*1.8+32 }
+        },
+        dist: {
+            format: '###.#in',
+            convert: function(v){ return v*0.3937 }
+        }
+    }
+}
+
+var selected_units = 'imperial'
+
 function getDataTable() {
     var data = new google.visualization.DataTable();
     data.addColumn('datetime', 'Date');
@@ -15,17 +40,20 @@ function getDataTable() {
     var adj_depth_filter = new Filter(6)
     for (i in readings) {
         readings[i][0] = new Date(readings[i][0]*1000)
+
+        // Temp readings
+        readings[i][1] = UNITS[selected_units].temp.convert(readings[i][1])
+        readings[i][2] = UNITS[selected_units].temp.convert(readings[i][2])
+
         // Snow depth
-        if (readings[i][3] == null) {
-            readings[i][3] = 0
-        }
-        readings[i][3] = depth_filter.add(readings[i][3])
+        d = readings[i][3] || 0
+        d = depth_filter.add(d)
+        readings[i][3] = UNITS[selected_units].dist.convert(d)
 
         // temp-corrected snow depth
-        if (readings[i][4] == null) {
-            readings[i][4] = 0
-        }
-        readings[i][4] = adj_depth_filter.add(readings[i][4])
+        d = readings[i][4] || 0
+        d = adj_depth_filter.add(d)
+        readings[i][4] = UNITS[selected_units].dist.convert(d)
     }
     data.addRows(readings)
     data.sort(0)
@@ -36,14 +64,12 @@ function getDataTable() {
     });
     date_formatter.format(data,0);
     var temp_formatter = new google.visualization.NumberFormat({
-        fractionDigits: 1,
-        suffix: '\u00B0C'
+        pattern: UNITS[selected_units].temp.format
     });
     temp_formatter.format(data, 1);
     temp_formatter.format(data, 2);
     var height_formatter = new google.visualization.NumberFormat({
-        fractionDigits: 0,
-        suffix: 'cm'
+        pattern: UNITS[selected_units].dist.format
     });
     height_formatter.format(data, 3);
     height_formatter.format(data, 4);
@@ -88,7 +114,7 @@ function drawVisualization() {
             // Use the same chart area width as the control for axis alignment.
             'chartArea': {'height': '95%', 'width': '80%'},
             'hAxis': {'textPosition': 'none'},
-            'vAxis': {'format':'##.#\u00B0C'},
+            'vAxis': {'format': UNITS[selected_units].temp.format},
             'series': [{'color': AIR_TEMP}, {'color':SNOW_TEMP}]
         },
        'view': {'columns': [0,1,2]}
@@ -102,7 +128,7 @@ function drawVisualization() {
             // Use the same chart area width as the control for axis alignment.
             'chartArea': {'height': '95%', 'width': '80%'},
             'hAxis': {'textPosition': 'in'},
-            'vAxis': {'format':'###cm'},
+            'vAxis': {'format': UNITS[selected_units].dist.format},
             'series': [{'color': SNOW_HEIGHT}, {'color':SNOW_HEIGHT_ADJ}]
         },
         'view': {'columns': [0,3,4]}
