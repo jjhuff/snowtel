282c961e2664178a30f89dcdbfd9eb22cb67857a
Justin Huff <jjhuff@mspin.net>
2014-01-01 18:28:13 -0800

    Add a module for batch jobs. Creat an export job

--- appengine/run_dev.sh
+++ appengine/run_dev.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 APP=$( dirname "${BASH_SOURCE[0]}" )
-~/google_appengine/dev_appserver.py $APP
+~/google_appengine/dev_appserver.py default.yaml batch.yaml dispatch.yaml
--- appengine/update_app.sh
+++ appengine/update_app.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 APP=$( dirname "${BASH_SOURCE[0]}" )
-~/google_appengine/appcfg.py --oauth2 update $APP
+~/google_appengine/appcfg.py --oauth2 update default.yaml batch.yaml
+~/google_appengine/appcfg.py --oauth2 update_dispatch $APP
