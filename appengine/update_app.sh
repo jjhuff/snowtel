03c2af05cf7ac68d71af34e938f4e17ada7009da
Justin Huff <jjhuff@mspin.net>
2014-10-19 21:22:16 -0700

    Updated for new googler cloud sdk

--- appengine/update_app.sh
+++ appengine/update_app.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 APP=$( dirname "${BASH_SOURCE[0]}" )
-~/google_appengine/appcfg.py --oauth2 update default.yaml batch.yaml
-~/google_appengine/appcfg.py --oauth2 update_dispatch $APP
+appcfg.py --oauth2 update default.yaml batch.yaml
+appcfg.py --oauth2 update_dispatch $APP
