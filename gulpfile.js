6c656d1c237f1abc67472d01ad723a9138db394c
Justin Huff <jjhuff@mspin.net>
2014-10-31 13:54:35 -0700

    Working on go converstion

---
+++ gulpfile.js
@@ -0,0 +1,171 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var size = require('gulp-size');
+//var clean = require('gulp-clean');
+var rename = require('gulp-rename');
+var minifyCSS = require('gulp-minify-css');
+var minifyHTML = require('gulp-minify-html');
+//var changed = require('gulp-changed');
+var compass = require('gulp-compass');
+var bowerFiles = require('main-bower-files');
+var filter = require('gulp-filter');
+var filelog = require('gulp-filelog');
+var rev = require('gulp-rev');
+var ngHtml2Js = require("gulp-ng-html2js");
+var merge = require('merge-stream');
+var lazypipe = require('lazypipe');
+//var open = require('open');
+//var runSequence = require('run-sequence');
+//var connect = require('gulp-connect');
+var shell = require('gulp-shell');
+
+process.env.GOPATH = process.cwd();
+
+var paths = {
+    appjs: {
+        src: './src/snow.mspin.net/frontend/scripts/**/*.js',
+        dest: './src/snow.mspin.net/frontend/build/'
+    },
+    libjs: {
+        dest: './src/snow.mspin.net/frontend/build/'
+    },
+    views: {
+        src: './src/snow.mspin.net/frontend/views/*.html',
+        base: './src/snow.mspin.net/frontend/views/',
+        dest: './src/snow.mspin.net/frontend/build/'
+    },
+    styles: {
+        src: './src/snow.mspin.net/frontend/styles/*.scss',
+        dest: './src/snow.mspin.net/frontend/build/',
+        sass: 'src/snow.mspin.net/frontend/styles/',
+        import_path: ['./src/snow.mspin.net/frontend/vendor']
+    },
+    ae_extra: './src/snow.mspin.net/frontend/',
+    ae_yaml: {
+        dispatch: ['./src/snow.mspin.net/frontend/dispatch.yaml'],
+        prod: ['./src/snow.mspin.net/frontend/app-prod.yaml'],
+        dev: ['./src/snow.mspin.net/frontend/app-dev.yaml'],
+    },
+};
+
+var options = {
+  open: true,
+  httpPort: 4400,
+  devserver_port: 8080,
+  admin_port: 8000
+};
+
+// Helper function to generate a pipe that does the common file revisions
+function revFiles(name, dest) {
+    return lazypipe()
+        .pipe(size, {title: name})
+        .pipe(rename, { suffix: '.min' })
+        .pipe(rev)
+        .pipe(gulp.dest, dest)
+        .pipe(rev.manifest)
+        .pipe(rename, name + "-manifest.json")
+        .pipe(gulp.dest, dest);
+}
+
+// process the compass files
+gulp.task('styles', function () {
+    return gulp.src(paths.styles.src)
+        .pipe(compass({
+            css: paths.styles.dest,
+            sass: paths.styles.sass,
+            import_path: paths.styles.import_path
+        }))
+        .pipe(gulp.dest(paths.styles.dest))
+        .pipe(minifyCSS())
+        .pipe(size({title:"styles"}))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest(paths.styles.dest))
+        .pipe(rev())
+        .pipe(gulp.dest(paths.styles.dest))
+        .pipe(rev.manifest())
+        .pipe(rename("css-manifest.json"))
+        .pipe(gulp.dest(paths.styles.dest));
+});
+
+gulp.task('minify-appjs', function () {
+    var appjs = gulp.src(paths.appjs.src);
+    var viewjs = gulp.src(paths.views.src)
+        .pipe(minifyHTML({
+            empty: true,
+            quotes: true
+        }))
+        .pipe(ngHtml2Js({
+            moduleName: "app",
+            stripPrefix: paths.views.base,
+            prefix: "/_/views/"
+        }));
+
+    return merge(appjs, viewjs)
+        .pipe(concat("app.js"))
+        .pipe(gulp.dest(paths.appjs.dest))
+        .pipe(uglify())
+        .pipe(revFiles("appjs", paths.appjs.dest)());
+});
+
+gulp.task('minify-libjs', function () {
+    return gulp.src(bowerFiles()).pipe(filter([
+            '**/*.js',
+            '!bootstrap.js',
+            '!angular.js',
+            '!jquery.js',
+        ]))
+        //.pipe(filelog())
+        .pipe(concat("lib.js"))
+        .pipe(gulp.dest(paths.libjs.dest))
+        .pipe(uglify())
+        .pipe(revFiles("libjs", paths.libjs.dest)());
+});
+
+gulp.task('build', ['styles', 'minify-appjs', 'minify-libjs']);
+
+gulp.task('watch', ['build'], function() {
+    gulp.watch(paths.styles.src, ['styles']);
+    gulp.watch(paths.views.src, ['minify-appjs']);
+    gulp.watch(paths.appjs.src, ['minify-appjs']);
+    gulp.watch('./bower.json', ['minify-libjs']);
+});
+
+gulp.task('server', ['watch'], function(){
+    var cfg = [].concat(paths.ae_yaml.dev, paths.ae_yaml.dispatch);
+    gulp.src('').pipe(shell([
+        'goapp serve --port='+options.devserver_port+' --admin_port='+options.admin_port+' ' + cfg.join(' ')
+    ]));
+});
+
+gulp.task('deploy', ['build'], function(){
+    var appcfg = 'appcfg.py --oauth2 ';
+    gulp.src('').pipe(shell([
+        appcfg + 'update_cron ' + paths.ae_extra,
+        appcfg + 'update ' + paths.ae_yaml.prod.join(' '),
+        appcfg + 'update_dispatch ' + paths.ae_extra,
+    ]));
+});
+
+gulp.task('vet', shell.task([
+  'go vet `go list snow.mspin.net/... | grep -v third_party`',
+],{ignoreErrors:true}));
+
+gulp.task('goget', shell.task([
+  'goapp get snow.mspin.net/...',
+], {ignoreErrors: true}));
+
+gulp.task('test', shell.task([
+  'goapp test -parallel 4  `go list snow.mspin.net/... | grep -v third_party`',
+],{ignoreErrors:true}));
+
+gulp.task('install', ['goget'], function() {
+  return bower.commands.install()
+    .on('log', function(data) {
+      gutil.log('bower', gutil.colors.cyan(data.id), data.message);
+    });
+});
+
+// default gulp task
+gulp.task('default', ['server'], function() {
+});
