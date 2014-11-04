874cc043956fed0ac594d04b6d1cf9aba31ef860
Justin Huff <jjhuff@mspin.net>
2014-11-03 22:57:39 -0800

    Change dev app name

--- appengine/update_app.sh
+++ appengine/update_app.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 APP=$( dirname "${BASH_SOURCE[0]}" )
-appcfg.py --oauth2 update default.yaml batch.yaml
-appcfg.py --oauth2 update_dispatch $APP
+appcfg.py --oauth2 -A methowsnow update default.yaml batch.yaml
+appcfg.py --oauth2 -A methowsnow update_dispatch $APP
