444ea57ed99e21fc24329d3f4bb206b3547c5481
Justin Huff <jjhuff@mspin.net>
2014-11-03 22:57:52 -0800

    Refresh every 5min

--- src/snow.mspin.net/frontend/scripts/controllers/sensor.js
+++ src/snow.mspin.net/frontend/scripts/controllers/sensor.js
@@ -1,16 +1,34 @@
 'use strict';
 
 angular.module('app')
-  .controller('SensorCtrl', ['$scope', '$stateParams', '$q', 'Sensor', function ($scope, $stateParams, $q, Sensor) {
+.controller('SensorCtrl', ['$scope', '$stateParams', '$q', '$interval', 'Sensor', 'Readings', function ($scope, $stateParams, $q, $interval, Sensor, Readings) {
+    var stop;
+
     google.load('visualization', '1.0', {packages: ['corechart', 'controls']});
 
     $scope.sensor = Sensor.get({id:$stateParams.id});
-    $scope.readings = Sensor.query({id:$stateParams.id, collection:'readings'});
-    $scope.readings.$promise.then(function(readings){
-        if (readings && readings.length>0) {
-            $scope.current = readings[0];
+
+    function refresh() {
+        $scope.readings = Readings.get({id:$stateParams.id});
+        $scope.readings.$promise.then(function(readings){
+            if (readings && readings.length>0) {
+                $scope.current = readings[0];
+            }
+            window.drawVisualization($scope.readings)
+        });
+    }
+
+    stop = $interval(function() {
+        console.log("refresh");
+        refresh();
+    }, 5*60*1000);
+
+    $scope.$on('$destroy', function() {
+        if (angular.isDefined(stop)) {
+            $interval.cancel(stop);
+            stop = undefined;
         }
-        window.drawVisualization($scope.readings)
     });
 
-  }]);
+    refresh();
+}]);
--- src/snow.mspin.net/frontend/scripts/services/sensor.js
+++ src/snow.mspin.net/frontend/scripts/services/sensor.js
@@ -1,12 +1,17 @@
 'use strict';
 
-angular.module('app')
-.factory('Sensor', ['$resource', function($resource){
-    var SensorResource = $resource('/_/api/v1/sensors/:id/:collection', {id:'@id'}, {
+angular.module('app').factory('Sensor', ['$resource', function($resource){
+    var SensorResource = $resource('/_/api/v1/sensors/:id', {id:'@id'}, {
         'get': { method: "GET" },
         'query':  { method:'GET', isArray:true},
     });
-
     return SensorResource;
 }]);
 
+angular.module('app').factory('Readings', ['$resource', function($resource){
+    var ReadingsResource = $resource('/_/api/v1/sensors/:id/readings', {id:'@id'}, {
+        'get':  { method:'GET', isArray:true},
+    });
+
+    return ReadingsResource;
+}]);
