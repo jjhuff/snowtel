083e9c168c9bec49d1747b27dbbb7c23261f0ad9
Justin Huff <jjhuff@mspin.net>
2014-11-09 22:17:16 -0800

    Basic posting of reading data

--- src/snow.mspin.net/frontend/sensor_api.go
+++ src/snow.mspin.net/frontend/sensor_api.go
@@ -4,6 +4,7 @@ import (
 	"appengine"
 	"appengine/datastore"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/ant0ine/go-json-rest/rest"
@@ -20,15 +21,30 @@ type Sensor struct {
 const sensorEntityKind = "Sensor"
 
 type Reading struct {
-	Timestamp   time.Time `json:"timestamp" datastore:"timestamp"`
-	AmbientTemp float32   `json:"ambient_temp" datastore:"ambient_temp,noindex"`
-	SurfaceTemp float32   `json:"surface_temp" datastore:"surface_temp,noindex"`
-	HeadTemp    float32   `json:"head_temp" datastore:"head_temp,noindex"`
-	StationTemp float32   `json:"station_temp" datastore:"station_temp,noindex"`
-	SnowDepth   float32   `json:"snow_depth" datastore:"snow_depth,noindex"`
+	Sensor      *datastore.Key `json:"-" datastore:"sensor"`
+	Timestamp   time.Time      `json:"timestamp" datastore:"timestamp"`
+	AmbientTemp float32        `json:"ambient_temp" datastore:"ambient_temp,noindex"`
+	SurfaceTemp float32        `json:"surface_temp" datastore:"surface_temp,noindex"`
+	HeadTemp    float32        `json:"head_temp" datastore:"head_temp,noindex"`
+	StationTemp float32        `json:"station_temp" datastore:"station_temp,noindex"`
+	SnowDepth   float32        `json:"snow_depth" datastore:"snow_depth,noindex"`
 }
 
 const readingEntityKind = "Reading"
+
+func getSensor(ctx appengine.Context, id string) (Sensor, error) {
+	sensorKey := datastore.NewKey(ctx, sensorEntityKind, id, 0, nil)
+
+	var sensor Sensor
+	err := datastore.Get(ctx, sensorKey, &sensor)
+	sensor.Id = id
+	if err != nil {
+		if _, ok := err.(*datastore.ErrFieldMismatch); !ok {
+			return sensor, err
+		}
+	}
+	return sensor, nil
+}
 
 func (app *AppContext) GetSensors(w rest.ResponseWriter, req *rest.Request) {
 	ctx := appengine.NewContext(req.Request)
@@ -55,17 +71,12 @@ func (app *AppContext) GetSensor(w rest.ResponseWriter, req *rest.Request) {
 		rest.Error(w, "Missing sensor id", http.StatusBadRequest)
 		return
 	}
-	sensorKey := datastore.NewKey(ctx, sensorEntityKind, sensorId, 0, nil)
-
-	var sensor Sensor
-	err := datastore.Get(ctx, sensorKey, &sensor)
+	sensor, err := getSensor(ctx, sensorId)
 	if err != nil {
-		if _, ok := err.(*datastore.ErrFieldMismatch); !ok {
-			rest.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
+		rest.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
-	sensor.Id = sensorId
+
 	w.WriteJson(sensor)
 }
 
@@ -114,5 +125,55 @@ func (app *AppContext) PostReading(w rest.ResponseWriter, req *rest.Request) {
 		return
 	}
 
-	ctx.Infof("surface_temp=%s", req.FormValue("surface_temp"))
+	sensor, err := getSensor(ctx, sensorId)
+	if err != nil {
+		if err == datastore.ErrNoSuchEntity {
+			// TODO: make new sensor
+			ctx.Errorf("Unknown sensor")
+			rest.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		} else {
+			rest.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+
+	reading := Reading{
+		Sensor:    datastore.NewKey(ctx, sensorEntityKind, sensorId, 0, nil),
+		Timestamp: time.Now(),
+	}
+
+	// Helper to parse a float form field
+	getFloat := func(field string) (float32, error) {
+		f, err := strconv.ParseFloat(req.FormValue(field), 32)
+		if err != nil {
+			rest.Error(w, "Bad param: "+field, http.StatusBadRequest)
+			return 0, err
+		} else {
+			return float32(f), nil
+		}
+	}
+
+	if reading.AmbientTemp, err = getFloat("ambient_temp"); err != nil {
+		return
+	}
+	if reading.SurfaceTemp, err = getFloat("surface_temp"); err != nil {
+		return
+	}
+	if reading.HeadTemp, err = getFloat("head_temp"); err != nil {
+		return
+	}
+	var snow_dist float32
+	if snow_dist, err = getFloat("snow_dist"); err != nil {
+		return
+	}
+	reading.SnowDepth = sensor.Height - snow_dist
+
+	readingKey := datastore.NewIncompleteKey(ctx, readingEntityKind, nil)
+	_, err = datastore.Put(ctx, readingKey, &reading)
+	if err != nil {
+		rest.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	ctx.Infof("Reading: %v", reading)
 }
