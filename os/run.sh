eb0d9070eb62ba8146dccd01083e25992df76108
Justin Huff <jjhuff@mspin.net>
2016-01-03 16:47:16 -0800

    Cleanup

--- os/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#screen -dmS snowtel ./snowtel.py -p /dev/ttyAMA0 -r 300 -e 28.B2C050000000
-#screen -dmS snowtel ./snowtel.py -p /dev/ttyAMA0 -r 10 -i 2 -e 28.B2C050000000
-
--- os/tunnel.sh
+++
@@ -1,6 +0,0 @@
-wget -O /tmp/tunnel_key https://s3.amazonaws.com/temp.mspin.net/tunnel
-chmod 600 /tmp/tunnel_key
-while true; do
-    ssh -o StrictHostKeyChecking=no -nNT -R 2222:localhost:22 -i/tmp/tunnel_key tunnel@tunnel.mspin.net
-    sleep 1
-done
